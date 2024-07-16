f72869f519f5e10f3cc646ca7246bf5d142f675a
Lucasgrn <lucasgabriel.ogn@gmail.com>
2024-07-16 14:49:01 -0300

    feat: Aplicar novas funções à aplicação

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import { authRouter } from "./routes/auth.route.js"
+import { petRouter } from "./routes/pet.route.js";
+import { locationRouter } from "./routes/location.route.js";
 
 const app = express()
 app.use(express.json());
@@ -7,5 +9,7 @@ app.use(express.json());
 app.get('/', (req, res) => { res.send('Running') })
 
 app.use('/auth', authRouter)
+app.use('/pet', petRouter)
+app.use('/location', locationRouter)
 
 app.listen(1337, () => (console.log('Running')))  
