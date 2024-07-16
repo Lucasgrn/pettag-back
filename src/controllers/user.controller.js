bcab26962e9ec57132a92a6e4290b8d3dcd7aac4
Lucasgrn <lucasgabriel.ogn@gmail.com>
2024-07-16 14:47:32 -0300

    feat: Criptografia da senha e gerar JWT

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,19 @@
+import { compare, hash } from "bcrypt"
 import { prisma } from "../../prisma/index.js"
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req, res) => { // Cadastro de usuário
-  const {name, email, password, instagram, phone } = req.body
+  const { name, email, password, instagram, phone } = req.body
   try {
     if (await prisma.user.findUnique({ where: { email } })) { // Checa se já existe um usuário com o email digitado
       return res.status(422).json({ error: 'User already exists!' })
     } else {
+      const pswd = await hash(password, 12)
       const user = await prisma.user.create({ // Cadastra usuário
         data: {
           name,
           email,
-          password,
+          password: pswd,
           instagram,
           phone
         }
@@ -26,11 +29,13 @@ export const login = async (req, res) => { // Login de usuário
   const { email, password } = req.body
   try {
     const user = await prisma.user.findUnique({ where: { email } })
-    if (!user || user.password !== password) { // Compara as credenciais
-      return res.status(401).json({ error: "Email or password invalid!" })
+    if (!user || !await compare(password, user.password)) { // Compara as credenciais
+      return res.status(401).json({ error: "Email or password incorrect!" })
     } else {
-      const token = 'placeholder' // Gerar token de acesso
-      return res.status(200).json({ user, token })
+      const accessToken = jwt.sign({ id: user.id }, process.env.PRIVATE_KEY, {
+        expiresIn: "7 days"
+      })
+      return res.status(200).json({ user, accessToken })
     }
   } catch (error) {
     return res.status(500).json({ error })
