ed5b101db65f80ed1ecd8a8f4d9395e9dfebfff3
Lucasgrn <lucasgabriel.ogn@gmail.com>
2024-07-26 16:13:05 -0300

    feat: Rota para retornar informações de um Pet específico

--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -34,6 +34,24 @@ export const listPets = async (req, res) => { // Lista os Pets do usuário logad
   }
 }
 
+export const getPet = async (req, res) => {
+  const { token } = req.params
+  try {
+    const pet = await prisma.pet.findUnique({
+      where: {
+        token
+      }
+    })
+    if (pet) {
+      return res.status(200).json(pet)
+    } else {
+      return res.status(404).json({ error: 'Pet not found!' })
+    }
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
+
 export const editPet = async (req, res) => { // Editar campos de um Pet via token
   const {
     token,
--- src/routes/pet.route.js
+++ src/routes/pet.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deletePet, editPet, listPets, petRegister } from "../controllers/pet.controller.js";
+import { deletePet, editPet, getPet, listPets, petRegister } from "../controllers/pet.controller.js";
 import authMid from "../middlewares/auth.js";
 
 const petRouter = Router()
@@ -9,6 +9,7 @@ petRouter.use(authMid)
 petRouter.post('/', petRegister)
 petRouter.get('/', listPets)
 petRouter.patch('/', editPet)
+petRouter.get('/:token', getPet)
 petRouter.delete('/:token', deletePet)
 
 export { petRouter }
