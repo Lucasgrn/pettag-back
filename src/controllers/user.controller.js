9c724fe4dc63106041579c2c0320f8ebbfbd03c3
Lucasgrn <lucasgabriel.ogn@gmail.com>
2024-08-02 11:09:39 -0300

    feat: Editar usuário cadastrado no banco

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -55,3 +55,16 @@ export const getSelfInfo = async (req, res) => {
     return res.status(500).json({ error })
   }
 }
+
+export const editUser = async (req, res) => {
+  try {
+    req.body.updatedAt = new Date()
+    const user = await prisma.user.update({
+      where: { id: req.userId },
+      data: req.body
+    })
+    res.status(200).json(user)
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authMid from "../middlewares/auth.js";
-import { getSelfInfo } from "../controllers/user.controller.js";
+import { editUser, getSelfInfo } from "../controllers/user.controller.js";
 const userRouter = Router()
 userRouter.use(authMid)
 
 userRouter.get('/', getSelfInfo)
+userRouter.patch('/', editUser)
 
 export { userRouter }
